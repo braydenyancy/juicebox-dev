89fbe58f7bafb2e866598f5c4c8f8caabf0e8d2a
braydenyancy <yancy.brayden@gmail.com>
2022-10-03 00:35:33 -0500

    part 2

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
-
 const morgan = require('morgan');
-server.use(morgan('dev'));
-server.use(express.json())
-
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
-
 const { client } = require('./db');
+
 client.connect();
 
-
-
+server.use(morgan('dev'));
+server.use(express.json())
 
 
 server.listen(PORT, () => {
@@ -25,4 +21,7 @@ server.use((req, res, next) => {
     console.log(req.body);
     console.log("<_____Body Logger END_____>");
     next();
-  });
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
